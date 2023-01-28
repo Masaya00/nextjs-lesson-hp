5d5b7d8b07266885f71ed0ccab93c494442a2e9c
masaya kato <masayakato@masayanoMacBook-Air.local>
2023-01-28 10:52:22 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Layout } from "@/components/Layout"
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen py-0 px-2 flex-col justify-center items-center'>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Layout title='TOP'>
+      <p className="text-4xl">Welcome to Nextjs</p>
+    </Layout>
   )
 }
